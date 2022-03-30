d49da45c20adf38e6270747b20224a050f60985d
Abhinab Mishra <88830300+abhinavm07@users.noreply.github.com>
2022-03-31 00:14:01 +0545

    Add files via upload

---
+++ boardingPage.js
@@ -0,0 +1,11 @@
+const trafficButtonEl=document.getElementById("traffic-id");
+
+const userButtonEl = document.getElementById("user-id");
+
+trafficButtonEl.addEventListener("click",p=>{
+    console.log("Traffic Button Clicked");
+})
+
+userButtonEl.addEventListener("click",u=>{
+    console.log("User Button Clicked");
+})
