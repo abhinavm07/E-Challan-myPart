41dd17477cb18082774156b2f80ff3f18acd681a
Abhinab Mishra <88830300+abhinavm07@users.noreply.github.com>
2022-04-06 07:11:44 +0545

    Add files via upload

--- adminPanel.js
+++ adminPanel.js
@@ -22,14 +22,72 @@ const challanEl = document.getElementById("challan-id");
 
 const licenseEl = document.getElementById("license-num");
 
+const vehicleEl = document.getElementById("vehicle-number");
+
+const usernameEl = document.getElementById("username");
+
+const trafficIDEl = document.getElementById("traffic-id");
+
+const fineAmountEl = document.getElementById("fine-amount");
+
+const checkboxEl = document.getElementById("checkbox");
+
 const previewChallanEl = document.getElementById("previewChallanID");
+
+const previewLicensenEl = document.getElementById("previewLicenceNum");
+
+const previewVehicleEl = document.getElementById("previewVehicleNum");
+
+const previewUsernameEl = document.getElementById("previewUsername");
+
+const previewtrafficIDEl = document.getElementById("previewTrafficId");
+
+const previewfineAmountEl = document.getElementById("previewFineAmount");
+
+const challanItems = {
+  challanMan: [challanEl, previewChallanEl],
+  license: [licenseEl, previewLicensenEl],
+  vehicle: [vehicleEl, previewVehicleEl],
+  username: [usernameEl, previewUsernameEl],
+  trafficID: [trafficIDEl, previewtrafficIDEl],
+  fineAmount: [fineAmountEl, previewfineAmountEl],
+};
 
 let userInput = "";
 
-licenseEl.addEventListener("click", challan);
-function challan() {
-  if (userInput != challanEl.value) {
-    previewChallanEl.textContent = `Challan ID : ${challanEl.value}`;
-    userInput = challanEl.value;
+licenseEl.addEventListener("click", (l) => {
+  previewChallan(challanItems, "challanMan", "Challan ID : ");
+});
+
+vehicleEl.addEventListener("click", (v) => {
+  previewChallan(challanItems, "license", "License Number : ");
+});
+
+usernameEl.addEventListener("click", (u) => {
+  previewChallan(challanItems, "vehicle", "Vehicle Number : ");
+});
+
+trafficIDEl.addEventListener("click", (t) => {
+  previewChallan(challanItems, "username", "Username  : ");
+});
+
+fineAmountEl.addEventListener("click", (f) => {
+  previewChallan(challanItems, "trafficID", "Traffic ID : ");
+});
+
+checkboxEl.addEventListener("click", (ce) => {
+  previewChallan(challanItems, "fineAmount", "Fine Amount :");
+});
+
+function previewChallan(previewChallanElement, changingContent, innertextMan) {
+  console.log(previewChallanElement[changingContent][0].value);
+  console.log(userInput);
+  if (userInput != previewChallanElement[changingContent][0].value) {
+    console.log(previewChallanElement[changingContent][1]);
+    previewChallanElement[
+      changingContent
+    ][1].textContent = `${innertextMan}${previewChallanElement[changingContent][0].value}`;
+    userInput = previewChallanElement[changingContent][0].value;
+    console.log("Abhinav");
   }
 }
